4432b37a6f4e2d139903d8b99c58fd33773ee43c
Sivaani <sivaanijanaswamy@gmail.com>
2025-08-04 23:41:11 +0530

    new folders

--- Java Threads/CreateThreads.java
+++ Java Threads/CreateThreads.java
@@ -5,6 +5,7 @@ public class CreateThreads implements Runnable{
     thread.start();
     System.out.println("This code is outside of the thread");
     }
+    
     public void run(){
         System.out.println("This code is running in a thread");
     }
