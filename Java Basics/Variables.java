aef434c8b0b5c2c0cbdedbdc9c12e0497857442b
Sivaani <sivaanijanaswamy@gmail.com>
2025-08-04 20:02:20 +0530

    Add Java Basics

---
+++ Java Basics/Variables.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Variables {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        String name;
+        int age;
+        float cgpa;
+        char grade;
+        boolean result;
+        System.out.println("Enter name:");
+        name = sc.nextLine();
+        System.out.println("Enter age:");
+        age = sc.nextInt();
+        System.out.println("Enter cgpa:");
+        cgpa = sc.nextFloat();
+        System.out.println("Enter grade:");
+        grade = sc.next().charAt(0);
+        System.out.println("Enter pass/fail (True/False):\n");
+        result = sc.nextBoolean();
+        int numberGrade = (int)cgpa;
+        System.out.printf(" Name:%s \n Age:%d \n CGPA:%f \n Grade:%c \n Pass:%b \n Number Grade: %d",name,age,cgpa,grade,result,numberGrade);
+        sc.close();
+    }
+}
