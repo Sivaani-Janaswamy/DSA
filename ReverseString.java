eb0966cbbd478161486d35b6fac72e288e69dc71
Sivaani <sivaanijanaswamy@gmail.com>
2025-08-05 12:00:25 +0530

    modified files

--- ReverseString.java
+++ ReverseString.java
@@ -4,7 +4,11 @@ public class ReverseString{
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter String: ");
         String str = sc.nextLine();
-
+        String rev = "";
+        for(int i = str.length()-1;i>=0;i--){
+            rev = rev+str.charAt(i);
+        }
+        System.out.println(rev);
         sc.close();
     }
 }
