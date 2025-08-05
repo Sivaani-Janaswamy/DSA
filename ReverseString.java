c3be693d64c014357095bc60b0bffc7d64e7f13c
Sivaani <sivaanijanaswamy@gmail.com>
2025-08-05 12:06:13 +0530

    add reverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -1,14 +1,15 @@
 import java.util.*;
 public class ReverseString{
+    static final Scanner sc = new Scanner(System.in);  
     public static void main(String[]args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter String: ");
-        String str = sc.nextLine();
-        String rev = "";
-        for(int i = str.length()-1;i>=0;i--){
-            rev = rev+str.charAt(i);
+        try (sc) {
+            System.out.println("Enter String: ");
+            String str = sc.nextLine();
+            String rev = "";
+            for(int i = str.length()-1;i>=0;i--){
+                rev = rev+str.charAt(i);
+            }
+            System.out.println(rev);
         }
-        System.out.println(rev);
-        sc.close();
     }
 }
