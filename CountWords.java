a6ae27e98eb65ec06e0f6a11e76a0d1d99c71d11
Sivaani <sivaanijanaswamy@gmail.com>
2025-08-05 11:18:49 +0530

    new files

---
+++ CountWords.java
@@ -0,0 +1,12 @@
+package DSA;
+import java.util.*;
+public class CountWords {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Sentence:");
+        String sentence = sc.nextLine();
+        int noOfWords = sentence.split("\\s").length;
+        System.out.printf("No of words: %d\n",noOfWords);
+        sc.close();
+    }
+}
