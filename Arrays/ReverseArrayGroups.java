5ec09e087951e210140d3fa67fd612cb5e34901e
Sivaani <sivaanijanaswamy@gmail.com>
2025-08-09 18:03:29 +0530

    Add ReverseArrayGroups.java

---
+++ Arrays/ReverseArrayGroups.java
@@ -0,0 +1,30 @@
+public class ReverseArrayGroups {
+    public void reverseGroups(int[] arr,int k){
+        int start = 0,end, temp;
+        int left, right;
+        int len = arr.length;
+        while(start<len){
+            end = Math.min(start + k - 1, len - 1);
+            left = start;                  
+            right = end;
+            while(left<right){
+                temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+            start+=k;
+            end = Math.min(start+k-1,len-1);
+        }
+    }
+    public static void main(String[]args){
+        ReverseArrayGroups rag = new ReverseArrayGroups();
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int k = 3;
+        rag.reverseGroups(arr, k);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
