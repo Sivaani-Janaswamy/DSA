e5106580761874a884427d121c7b9a9f5186a717
Sivaani <sivaanijanaswamy@gmail.com>
2025-08-09 10:12:59 +0530

    add implementation files

---
+++ Implementations/HashMap.java
@@ -0,0 +1,43 @@
+class Entry {
+    int key, value;
+    Entry next;
+    Entry(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
+class HashMap {
+    private final int SIZE = 100;
+    private Entry[] table = new Entry[SIZE];
+
+    int hash(int key) {
+        return key % SIZE;
+    }
+
+    void put(int key, int value) {
+        int idx = hash(key);
+        Entry head = table[idx];
+        while (head != null) {
+            if (head.key == key) {
+                head.value = value;
+                return;
+            }
+            head = head.next;
+        }
+        Entry newEntry = new Entry(key, value);
+        newEntry.next = table[idx];
+        table[idx] = newEntry;
+    }
+
+    Integer get(int key) {
+        int idx = hash(key);
+        Entry head = table[idx];
+        while (head != null) {
+            if (head.key == key)
+                return head.value;
+            head = head.next;
+        }
+        return null;
+    }
+}
