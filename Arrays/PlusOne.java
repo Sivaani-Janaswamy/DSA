0d13b3f419098426456552a661cc6eb798596be7
Sivaani <sivaanijanaswamy@gmail.com>
2025-08-11 17:32:42 +0530

    add PlusOne.java

--- Arrays/PlusOne.java
+++ Arrays/PlusOne.java
@@ -1,3 +1,27 @@
 public class PlusOne {
-    
+    public int[] plusOne(int[] arr){
+        int carry = 1;
+        int sum;
+        for(int i = arr.length-1;i>=0;i--){
+            sum = arr[i]+carry;
+            arr[i] = sum%10;
+            carry = sum/10;
+        }
+        if(carry>0){
+            int[] newArr = new int[arr.length+1];
+            newArr[0] = carry;
+            System.arraycopy(arr,0,newArr,1,arr.length);
+            return newArr;
+        }
+        return arr;
+    }
+    public static void main(String[]args){
+        int[] arr = {9,9,9};
+        PlusOne po = new PlusOne();
+        int[] result = po.plusOne(arr);
+        for(int i:result){
+            System.out.print(i+" ");
+        }
+
+    }
 }
