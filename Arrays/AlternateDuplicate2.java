fddf4b6e54ccf91a34a3fb0de17fe38e4025460f
Sivaani <sivaanijanaswamy@gmail.com>
2025-08-13 09:15:42 +0530

    alternateduplicate2.java

---
+++ Arrays/AlternateDuplicate2.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class AlternateDuplicate2 {
+    public static ArrayList<Integer> duplicateK(ArrayList<Integer> arr, int k) {
+        int n = arr.size();
+        for (int i = 0; i < n; i++) {
+            if (arr.get(i) == k) {
+                // Insert an adjacent k
+                arr.add(i + 1, k);
+                i++;
+
+                // Remove the last element
+                arr.remove(arr.size() - 1);
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(1, 0, 2, 3, 0, 4, 5, 0));
+        int k = 0;
+        ArrayList<Integer> ans = duplicateK(arr, k);
+
+        for (int num : ans) {
+            System.out.print(num + " ");
+        }
+    }
+}
